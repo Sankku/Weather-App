33d6c6fa57a6f9c761f146048d8690a35815f399
Sankku <santiagoemprendedor9@gmail.com>
2022-07-18 07:11:30 -0300

    Setting up login

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,59 @@
-const fetchWeather = "/weather"
+const fetchWeather = "/weather";
 
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
-const weatherIcon = document.querySelector('.weatherIcon i');
-const weatherCondition = document.querySelector('.weatherCondition');
-const tempElement = document.querySelector('.temperature span');
-const locationElement = document.querySelector('.place');
+const weatherIcon = document.querySelector(".weatherIcon i");
+const weatherCondition = document.querySelector(".weatherCondition");
+const tempElement = document.querySelector(".temperature span");
+const locationElement = document.querySelector(".place");
 // Get Date
-const dateElement = document.querySelector('.date');
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
-dateElement.textContent = new Date().getDate() + ", " + monthNames[new Date().getMonth()];
+const dateElement = document.querySelector(".date");
 
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
+dateElement.textContent =
+  new Date().getDate() +
+  ", " +
+  monthNames[new Date().getMonth()].substring(0, 3);
 
-
-weatherForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    locationElement.textContent = "Loading...";
-    tempElement.textContent = "";
-    weatherCondition.textContent = "";
-    const locationApi = fetchWeather + "?address=" + search.value;
-    fetch(locationApi).then(response => {
-        response.json().then(data => {
-            if(data.error) {
-                locationElement.textContent = data.error;
-                tempElement.textContent = "";
-                weatherCondition.textContent = "";
-            } else {
-                if(data.description === "rain" || data.description === "fog") {
-                    weatherIcon.className = "wi wi-day-" + data.description
-                } else {
-                    weatherIcon.className = "wi wi-day-cloudy"
-                }
-                locationElement.textContent = data.cityName;
-                tempElement.textContent = (data.temperature - 273.5).toFixed(2) + String.fromCharCode(176);
-                weatherCondition.textContent = data.description.toUpperCase();          
-            }
-        }) 
-    }) 
-})
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  locationElement.textContent = "Loading...";
+  tempElement.textContent = "";
+  weatherCondition.textContent = "";
+  const locationApi = fetchWeather + "?address=" + search.value;
+  fetch(locationApi).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        locationElement.textContent = data.error;
+        tempElement.textContent = "";
+        weatherCondition.textContent = "";
+      } else {
+        console.log();
+        if (data.description === "rain" || data.description === "fog") {
+          weatherIcon.className = "wi wi-day-" + data.description;
+        } else {
+          weatherIcon.className = "wi wi-day-cloudy";
+        }
+        locationElement.textContent = data.cityName;
+        tempElement.textContent =
+          (data.temperature - 273.5).toFixed(2) + String.fromCharCode(176);
+        weatherCondition.textContent = data.description.toUpperCase();
+      }
+    });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,68 +1,95 @@
-const express = require('express');
+const express = require("express");
 const hbs = require("hbs");
-const passport = require("passport")
+const bcrypt = require("bcrypt");
 const path = require("path");
 const app = express();
 
-const weatherData = require('../utils/weatherData');
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+initializePassport(passport);
+// Local variable instead of entire database
+const users = [];
 
-const port = process.env.PORT || 3000
+const weatherData = require("../utils/weatherData");
 
-const publicStaticDirPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
 
-const viewsPath = path.join(__dirname, '../templates/views');
+const publicStaticDirPath = path.join(__dirname, "../public");
 
-const partialsPath = path.join(__dirname, '../templates/partials');
+const viewsPath = path.join(__dirname, "../templates/views");
 
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 app.use(express.static(publicStaticDirPath));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/login", (req, res) => {
-    // Mostrar el formulario de Login
-    res.render("login");
-})
-
-app.get('', (req, res) => {
-    res.render('index.hbs', {
-        title: 'Weather App'
-    });
+  // Mostrar el formulario de Login
+  res.render("login");
 });
 
+app.post("/login", (req, res) => {});
 
-//localhost:3000/weather?
-app.get('/weather', (req, res) => {
-    const address = req.query.address
-    if(!address) {
-        return res.send({
-            error: "You must enter address in search box"
-        })
+app.get("/register", (req, res) => {
+  // Mostrar el formulario de Login
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("index.hbs", {
+    title: "Weather App",
+  });
+});
+
+//localhost:3000/weather
+app.get("/weather", (req, res) => {
+  const address = req.query.address;
+  if (!address) {
+    return res.send({
+      error: "You must enter address in search box",
+    });
+  }
+
+  weatherData(address, (error, { temperature, description, cityName } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      });
     }
-
-    weatherData(address, (error, {temperature, description, cityName} = {}) => {
-        if(error) {
-            return res.send({
-                error
-            })
-        }
-        console.log(temperature, description, cityName);
-        res.send({
-            temperature,
-            description,
-            cityName
-        })
-    })
+    console.log(temperature, description, cityName);
+    res.send({
+      temperature,
+      description,
+      cityName,
+    });
+  });
 });
 
 app.get("*", (req, res) => {
-    res.render('404', {
-        title: "Page not found"
-    });
+  res.render("404", {
+    title: "Page not found",
+  });
 });
 
 app.listen(port, () => {
-    console.log("Server is up and running on port: ", port)
+  console.log("Server is up and running on port: ", port);
 });
